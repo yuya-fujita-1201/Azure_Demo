74ccf3dd08ff198ef34225ce4f1e125a91f521c7
Yuya Fujita <sam.y.1201@gmail.com>
2023-06-20 01:20:58 +0900

    repair

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,23 @@
-// server.ts
 import express from 'express';
 import { ODataServer, ODataController, odata, ODataQuery } from 'odata-v4-server';
 import { connectToSql } from './OdataService';
 import { MyTableType, UserDetails } from './models';
 import sql from 'mssql';
 
-@odata.type(MyTableType)
 class MyTableController extends ODataController {
-    // ...
+    @odata.GET
+    public async get(@odata.query query: ODataQuery): Promise<MyTableType[]> {
+        await connectToSql();
+        const request = new sql.Request();
+        const result = await request.query(`SELECT * FROM MyTableType`);
+        return result.recordset.map((record: any) => Object.assign(new MyTableType(), record));
+    }
 }
 
-@odata.type(UserDetails)
 class UserDetailsController extends ODataController {
     @odata.GET
     public async get(@odata.query query: ODataQuery): Promise<UserDetails[]> {
+        await connectToSql();
         const request = new sql.Request();
         const result = await request.query(`SELECT * FROM UserDetails`);
         return result.recordset.map((record: any) => Object.assign(new UserDetails(), record));
@@ -21,22 +25,16 @@ class UserDetailsController extends ODataController {
 }
 
 @odata.namespace('mySampleDatabase')
+@odata.container('myContainer')
 class MyODataServer extends ODataServer {}
 
-MyODataServer.addController(MyTableController, 'MyTableType');
-MyODataServer.addController(UserDetailsController, 'UserDetails');
-
-// Remaining code...
+MyODataServer.addController(MyTableController, '/MyTableType');
+MyODataServer.addController(UserDetailsController, '/UserDetails');
 
 const app = express();
 
-// Connect to SQL Server
-connectToSql();
-
-// Setup OData middleware
 app.use('/odata', MyODataServer.create());
 
-// Start the server
 app.listen(80, () => {
     console.log('Server is running at http://localhost:80');
 });
