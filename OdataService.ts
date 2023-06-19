adaa7c132d16fea1d758de63e042f05ea059b20a
Yuya Fujita <sam.y.1201@gmail.com>
2023-06-19 21:13:03 +0900

    ソースを３分割

--- OdataService.ts
+++ OdataService.ts
@@ -1,6 +1,5 @@
-import express from 'express';
+// OdataService.ts
 import sql from 'mssql';
-import { ODataServer, ODataController, Edm, odata, ODataQuery } from 'odata-v4-server';
 
 // SQL Server connection config
 const sqlConfig = {
@@ -12,50 +11,4 @@ const sqlConfig = {
 };
 
 // Make sure to connect to the database
-sql.connect(sqlConfig).catch(err => console.error('Failed to connect to the database:', err));
-
-// Define entity model
-@odata.namespace('mySampleDatabase')
-@odata.type('MyTableType')  // change class name to 'MyTableType'
-class MyTableType {         // change class name to 'MyTableType'
-    @Edm.Key
-    @Edm.Computed
-    @Edm.Int32
-    public KeyValue: number = 0;
-
-    @Edm.String
-    public Item1: string = "";
-
-    @Edm.String
-    public Item2: string = "";
-
-    @Edm.String
-    public Item3: string = "";
-}
-
-// Define controller for the entity
-class MyTableController extends ODataController {
-    @odata.GET
-    public async get(@odata.query query: ODataQuery): Promise<MyTableType[]> {  // change return type to 'MyTableType[]'
-        const request = new sql.Request();
-        const result = await request.query(`SELECT * FROM MyTable`);
-        return result.recordset.map((record: any) => Object.assign(new MyTableType(), record));  // change 'new MyTable()' to 'new MyTableType()'
-    }
-}
-
-// Define the server
-@odata.namespace('mySampleDatabase')
-class MyODataServer extends ODataServer {}
-
-// Add controller to the server
-MyODataServer.addController(MyTableController, 'MyTable');
-
-const app = express();
-
-// Setup OData middleware
-app.use('/odata', MyODataServer.create());
-
-// Start the server
-app.listen(80, () => {
-    console.log('Server is running at http://localhost:80');
-});
+export const connectToSql = () => sql.connect(sqlConfig).catch(err => console.error('Failed to connect to the database:', err));
---
+++ models.ts
@@ -0,0 +1,19 @@
+// models.ts
+import { Edm, odata } from 'odata-v4-server';
+
+@odata.type('mySampleDatabase.MyTableType')
+export class MyTableType {
+    @Edm.Key
+    @Edm.Computed
+    @Edm.Int32
+    public KeyValue: number = 0;
+
+    @Edm.String
+    public Item1: string = "";
+
+    @Edm.String
+    public Item2: string = "";
+
+    @Edm.String
+    public Item3: string = "";
+}
